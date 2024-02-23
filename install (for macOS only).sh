c252dd368ea312c25c6fd892478d64c29f645e44
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2024-02-23 06:56:19 -0800

    Add files via upload

---
+++ install (for macOS only).sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -LO https://github.com/matthewyang204/Flappy-Owl/releases/download/1.0/Flappy.Owl.macOS.zip
+tar -xvf ~/Downloads/Flappy.Owl.macOS.zip
+mv ~/Downloads/Flappy.Owl.macOS/Flappy\ Owl.app /Applications/Flappy\ Owl.app
