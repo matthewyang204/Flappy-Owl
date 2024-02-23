0230825d38b6ae3ce1f69f3f6858ed072b466b8d
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2024-02-23 07:08:20 -0800

    Update install (for macOS only).sh

--- install (for macOS only).sh
+++ install (for macOS only).sh
@@ -2,4 +2,4 @@
 cd ~/Downloads
 curl -LO https://github.com/matthewyang204/Flappy-Owl/releases/download/1.0/Flappy.Owl.macOS.zip
 tar -xvf ~/Downloads/Flappy.Owl.macOS.zip
-mv ~/Downloads/Flappy.Owl.macOS/Flappy\ Owl.app /Applications/Flappy\ Owl.app
+mv ~/Downloads/Flappy\ Owl.app /Applications/Flappy\ Owl.app
